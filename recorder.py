3001df6c05369075f6b801504181239f39c69be8
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-14 19:45:43 +0100

    Working recorder

--- recorder.py
+++ recorder.py
@@ -1,24 +1,26 @@
 import pyaudio
 import wave
+import time
+import sys
 
 audio = pyaudio.PyAudio()
 
 stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
 
 frames = []
+filename = sys.argv[1]
 
-try:
-    while True:
-        data = stream.read(1024)
-        frames.append(data)
-except KeyboardInterrupt:
-    pass
+t_end = time.time() + 5
+while time.time() < t_end:
+    data = stream.read(1024)
+    frames.append(data)
+
 
 stream.stop_stream()
 stream.close()
 audio.terminate()
 
-sound_file = wave.open("myrecording.wav", "wb")
+sound_file = wave.open(filename + ".wav", "wb")
 sound_file.setnchannels(1)
 sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
 sound_file.setframerate(44100)
