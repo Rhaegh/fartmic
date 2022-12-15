8bf9e0459a7751a75af9a81dff03cdbaa8940173
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-15 20:36:12 +0100

    fix

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
- 
+
 import (
 	"fmt"
 	"log"
 	"net/http"
- 
+
 	"github.com/gorilla/websocket"
 )
- 
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
- 
- 
+
 func reader(conn *websocket.Conn) {
 	for {
 		// read in a message
@@ -25,40 +24,40 @@ func reader(conn *websocket.Conn) {
 		}
 		// print out that message for clarity
 		log.Println(string(p))
- 
+
 		if err := conn.WriteMessage(messageType, p); err != nil {
 			log.Println(err)
 			return
 		}
- 
+
 	}
 }
- 
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home Page")
 }
- 
+
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	// upgrade this connection to a WebSocket
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 	}
- 
+
 	log.Println("Client Connected")
-	err = ws.WriteMessage(1, &#91;]byte("Hi Client!"))
+	err = ws.WriteMessage(1, []byte("Hi Client!"))
 	if err != nil {
 		log.Println(err)
 	}
- 
+
 	reader(ws)
 }
- 
+
 func setupRoutes() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/ws", wsEndpoint)
 }
- 
+
 func main() {
 	fmt.Println("Hello World")
 	setupRoutes()
