392bbb32bfb81dfe1f76b31b83c4a25aef37a7e4
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-15 19:37:41 +0100

    Nieuwe recorder voor Pi

--- recorder.py
+++ recorder.py
@@ -1,28 +1,39 @@
 import pyaudio
 import wave
-import time
-import sys
 
-audio = pyaudio.PyAudio()
+form_1 = pyaudio.paInt16 # 16-bit resolution
+chans = 1 # 1 channel
+samp_rate = 44100 # 44.1kHz sampling rate
+chunk = 4096 # 2^12 samples for buffer
+record_secs = 3 # seconds to record
+dev_index = 2 # device index found by p.get_device_info_by_index(ii)
+wav_output_filename = 'test1.wav' # name of .wav file
 
-stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+audio = pyaudio.PyAudio() # create pyaudio instantiation
 
+# create pyaudio stream
+stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                    input_device_index = dev_index,input = True, \
+                    frames_per_buffer=chunk)
+print("recording")
 frames = []
-filename = sys.argv[1]
 
-t_end = time.time() + 5
-while time.time() < t_end:
-    data = stream.read(1024)
+# loop through stream and append audio chunks to frame array
+for ii in range(0,int((samp_rate/chunk)*record_secs)):
+    data = stream.read(chunk)
     frames.append(data)
 
+print("finished recording")
 
+# stop the stream, close it, and terminate the pyaudio instantiation
 stream.stop_stream()
 stream.close()
 audio.terminate()
 
-sound_file = wave.open(filename + ".wav", "wb")
-sound_file.setnchannels(1)
-sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
-sound_file.setframerate(44100)
-sound_file.writeframes(b''.join(frames))
-sound_file.close()
+# save the audio frames as .wav file
+wavefile = wave.open(wav_output_filename,'wb')
+wavefile.setnchannels(chans)
+wavefile.setsampwidth(audio.get_sample_size(form_1))
+wavefile.setframerate(samp_rate)
+wavefile.writeframes(b''.join(frames))
+wavefile.close()
