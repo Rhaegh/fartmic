efa018e0c3acebea8f129081b618c0eb558f9240
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-15 20:34:25 +0100

    New version ws

--- main.go
+++ main.go
@@ -1,90 +1,66 @@
 package main
-
+ 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"strings"
-
+ 
 	"github.com/gorilla/websocket"
 )
-
+ 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  4096,
-	WriteBufferSize: 4096,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
-var todoList []string
-
-func getCmd(input string) string {
-	inputArr := strings.Split(input, " ")
-	return inputArr[0]
-}
-
-func getMessage(input string) string {
-	inputArr := strings.Split(input, " ")
-	var result string
-	for i := 1; i < len(inputArr); i++ {
-		result += inputArr[i]
-	}
-	return result
-}
-
-func updateTodoList(input string) {
-	tmpList := todoList
-	todoList = []string{}
-	for _, val := range tmpList {
-		if val == input {
-			continue
-		}
-		todoList = append(todoList, val)
-	}
-}
-
-func main() {
-
-	http.HandleFunc("/todo", func(w http.ResponseWriter, r *http.Request) {
-		// Upgrade upgrades the HTTP server connection to the WebSocket protocol.
-		conn, err := upgrader.Upgrade(w, r, nil)
+ 
+ 
+func reader(conn *websocket.Conn) {
+	for {
+		// read in a message
+		messageType, p, err := conn.ReadMessage()
 		if err != nil {
-			log.Print("upgrade failed: ", err)
+			log.Println(err)
 			return
 		}
-		defer conn.Close()
-
-		// Continuosly read and write message
-		for {
-			mt, message, err := conn.ReadMessage()
-			if err != nil {
-				log.Println("read failed:", err)
-				break
-			}
-			input := string(message)
-			cmd := getCmd(input)
-			msg := getMessage(input)
-			if cmd == "add" {
-				todoList = append(todoList, msg)
-			} else if cmd == "done" {
-				updateTodoList(msg)
-			}
-			output := "Current Todos: \n"
-			for _, todo := range todoList {
-				output += "\n - " + todo + "\n"
-			}
-			output += "\n----------------------------------------"
-			message = []byte(output)
-			err = conn.WriteMessage(mt, message)
-			if err != nil {
-				log.Println("write failed:", err)
-				break
-			}
+		// print out that message for clarity
+		log.Println(string(p))
+ 
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
 		}
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "websockets.html")
-	})
-
-	http.ListenAndServe(":8080", nil)
+ 
+	}
+}
+ 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page")
+}
+ 
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	// upgrade this connection to a WebSocket
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+ 
+	log.Println("Client Connected")
+	err = ws.WriteMessage(1, &#91;]byte("Hi Client!"))
+	if err != nil {
+		log.Println(err)
+	}
+ 
+	reader(ws)
+}
+ 
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
+}
+ 
+func main() {
+	fmt.Println("Hello World")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
