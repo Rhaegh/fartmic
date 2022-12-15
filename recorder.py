c44b813e5d5995424a2bb2c3b1047c8970466efc
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-15 19:44:50 +0100

    .wav toegevoegd

--- recorder.py
+++ recorder.py
@@ -6,7 +6,7 @@ form_1 = pyaudio.paInt16 # 16-bit resolution
 chans = 1 # 1 channel
 samp_rate = 44100 # 44.1kHz sampling rate
 chunk = 4096 # 2^12 samples for buffer
-record_secs = 15 # seconds to record
+record_secs = 10 # seconds to record
 dev_index = 2 # device index found by p.get_device_info_by_index(ii)
 wav_output_filename = sys.argv[1] # name of .wav file
 
@@ -32,7 +32,7 @@ stream.close()
 audio.terminate()
 
 # save the audio frames as .wav file
-wavefile = wave.open(wav_output_filename,'wb')
+wavefile = wave.open(wav_output_filename".wav",'wb')
 wavefile.setnchannels(chans)
 wavefile.setsampwidth(audio.get_sample_size(form_1))
 wavefile.setframerate(samp_rate)
