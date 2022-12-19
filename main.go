86128e81c1e002a0c9fecc7d0712dce7a5bfb758
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-19 17:37:27 +0100

    database write test

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"os/exec"
+	"strconv"
 )
+
+type Scheetjes struct {
+	ID   string
+	Name string
+}
 
 func recordbutton() {
 	http.HandleFunc("/recordbutton", func(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +41,34 @@ func startrecorder() {
 	fmt.Println(string(stdout))
 }
 
+func writedatabase() {
+	records := []Scheetjes{
+		{"1", "Scheetje van Giel"},
+	}
+	file, err := os.Open("/home/pi/fartmic/data/database.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	w := csv.NewWriter(file)
+	defer w.Flush()
+	// Using Write
+	for _, record := range records {
+		row := []string{record.ID, record.Name}
+		if err := w.Write(row); err != nil {
+			log.Fatalln("error writing record to file", err)
+		}
+	}
+
+	// Using WriteAll
+	var data [][]string
+	for _, record := range records {
+		row := []string{record.ID, strconv.Itoa(record.Age)}
+		data = append(data, row)
+	}
+	w.WriteAll(data)
+}
+
 func main() {
-	newestfile()
+	writedatabase()
 	recordbutton()
 }
