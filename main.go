da3e926d4cd6a0c3e686644befa3164be5607208
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-19 16:55:07 +0100

    proberen is leren 2

--- main.go
+++ main.go
@@ -2,36 +2,16 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
-type ContactDetails struct {
-	Email   string
-	Subject string
-	Message string
-}
-
 func main() {
-	tmpl := template.Must(template.ParseFiles("webpage/index.html"))
-
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			tmpl.Execute(w, nil)
 			fmt.Println("Gelukt")
 			return
 		}
-
-		details := ContactDetails{
-			Email:   r.FormValue("email"),
-			Subject: r.FormValue("subject"),
-			Message: r.FormValue("message"),
-		}
-
-		// do something with details
-		_ = details
-
-		tmpl.Execute(w, struct{ Success bool }{true})
 	})
 
 	http.ListenAndServe(":8080", nil)
