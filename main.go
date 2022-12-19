e379837c2160849019a1358b9ab8f54b4a959ac4
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-19 17:20:34 +0100

    testnewestfile

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"os/exec"
 )
 
@@ -17,25 +19,40 @@ func recordbutton() {
 
 	http.ListenAndServe(":8080", nil)
 }
+func newestfile() {
+	dir := "/home/pi/fartmic/audiofiles/"
+	files, _ := ioutil.ReadDir(dir)
+	var newestFile string
+	var newestTime int64 = 0
+	for _, f := range files {
+		fi, err := os.Stat(dir + f.Name())
+		if err != nil {
+			fmt.Println(err)
+		}
+		currTime := fi.ModTime().Unix()
+		if currTime > newestTime {
+			newestTime = currTime
+			newestFile = f.Name()
+		}
+	}
+	fmt.Println(newestFile)
+}
 
 func startrecorder() {
 	app := "python"
-
 	arg0 := "recorder.py"
 	arg1 := "Hello world"
 
 	cmd := exec.Command(app, arg0, arg1)
 	stdout, err := cmd.Output()
-
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-
-	// Print the output
 	fmt.Println(string(stdout))
 }
 
 func main() {
 	recordbutton()
+	newestfile()
 }
