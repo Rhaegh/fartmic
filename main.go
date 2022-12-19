b22c2afc3d61be03e9a9b61a13cc1304e5b408b2
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-19 17:07:40 +0100

    recorder added to golang

--- main.go
+++ main.go
@@ -3,17 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os/exec"
 )
 
 func recordbutton() {
 	http.HandleFunc("/recordbutton", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
-			fmt.Println("Gelukt")
+			startrecorder()
+			fmt.Println("recorder function called")
 			return
 		}
 	})
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func startrecorder() {
+	cmd := exec.Command("recorder.py Bestandje")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(out))
+	fmt.Println("recording started")
 }
 
 func main() {
