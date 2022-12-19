854016c461ecec4ed4f05d1aa21c6093b36e3021
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-19 17:26:42 +0100

    removed bullshit

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"os"
 	"os/exec"
-	"path/filepath"
-	"strings"
 )
 
 func recordbutton() {
@@ -20,25 +16,6 @@ func recordbutton() {
 	})
 
 	http.ListenAndServe(":8080", nil)
-}
-func newestfile() {
-	dir := "/home/pi/fartmic/audiofiles/"
-	files, _ := ioutil.ReadDir(dir)
-	var newestFile string
-	var newestTime int64 = 0
-	for _, f := range files {
-		fi, err := os.Stat(dir + f.Name())
-		if err != nil {
-			fmt.Println(err)
-		}
-		currTime := fi.ModTime().Unix()
-		if currTime > newestTime {
-			newestTime = currTime
-			newestFile = f.Name()
-		}
-	}
-	newfilename := strings.TrimSuffix(newestFile, filepath.Ext(newestFile))
-	fmt.Println(newfilename)
 }
 
 func startrecorder() {
