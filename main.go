5139f98837026e3e496ed51ed10234c38b37897e
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-19 17:14:03 +0100

    test

--- main.go
+++ main.go
@@ -19,13 +19,23 @@ func recordbutton() {
 }
 
 func startrecorder() {
-	cmd := exec.Command("python recorder.py")
-	out, err := cmd.CombinedOutput()
+	app := "echo"
+
+	arg0 := "-e"
+	arg1 := "Hello world"
+	arg2 := "\n\tfrom"
+	arg3 := "golang"
+
+	cmd := exec.Command(app, arg0, arg1, arg2, arg3)
+	stdout, err := cmd.Output()
+
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
+		return
 	}
-	fmt.Println(string(out))
-	fmt.Println("recording started")
+
+	// Print the output
+	fmt.Println(string(stdout))
 }
 
 func main() {
