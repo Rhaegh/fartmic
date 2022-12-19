4e560155a9ef83c6d5dbb4503e0f8bd0d74aff59
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-19 16:59:52 +0100

    webpage fixed

--- webpage/script.js
+++ webpage/script.js
@@ -1,26 +1,10 @@
-var sock = null;
-var wsuri = "ws://192.168.72.131:9100/socket"; // <-- note new path
-
-window.onload = function() {
-
-	console.log("onload");
-
-	sock = new WebSocket(wsuri);
-
-	sock.onopen = function() {
-		console.log("connected to " + wsuri);
-	}
-
-	sock.onclose = function(e) {
-		console.log("connection closed (" + e.code + ")");
-	}
-
-	sock.onmessage = function(e) {
-		console.log("message received: " + e.data);
-	}
-};
-
 function send() {
-	var msg = document.getElementById('message').value;
-	sock.send(msg);
+	const Http = new XMLHttpRequest();
+	const url='http://192.168.72.131:8080/test';
+	Http.open("GET", url);
+	Http.send();
+	
+	Http.onreadystatechange = (e) => {
+	  console.log(Http.responseText)
+	}
 };
