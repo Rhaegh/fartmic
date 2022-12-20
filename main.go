fa791cea3b09f60b139f4869eb783feac58eb66e
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-20 22:27:30 +0100

    test

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+
+	"github.com/gocarina/gocsv"
 )
 
 func listener() {
@@ -29,29 +30,49 @@ func save(w http.ResponseWriter, req *http.Request) {
 	fmt.Println(name)
 }
 
-func writedatabase() {
-	f, err := os.OpenFile("/home/pi/fartmic/data/data.csv", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	data := [][]string{
-		{"id", "first_name", "last_name", "email"},
-	}
-	w := csv.NewWriter(f)
-	w.WriteAll(data)
-	w.Flush()
+type NotUsed struct {
+	Name string
 }
 
-func readdatabase() {
-	file, err := os.Open("/home/pi/fartmic/data/data.csv")
-	if err != nil {
-		fmt.Println(err)
-	}
-	reader := csv.NewReader(file)
-	records, _ := reader.ReadAll()
+type Client struct { // Our example struct, you can use "-" to ignore a field
+	Id            string  `csv:"client_id"`
+	Name          string  `csv:"client_name"`
+	Age           string  `csv:"client_age"`
+	NotUsedString string  `csv:"-"`
+	NotUsedStruct NotUsed `csv:"-"`
+}
 
-	fmt.Println(records[1])
+func writedatabase() {
+
+	clientsFile, err := os.OpenFile("/home/pi/fartmic/data/data.csv", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer clientsFile.Close()
+
+	clients := []*Client{}
+
+	if err := gocsv.UnmarshalFile(clientsFile, &clients); err != nil { // Load clients from file
+		panic(err)
+	}
+	for _, client := range clients {
+		fmt.Println("Hello", client.Name)
+	}
+
+	if _, err := clientsFile.Seek(0, 0); err != nil { // Go to the start of the file
+		panic(err)
+	}
+
+	clients = append(clients, &Client{Id: "12", Name: "John", Age: "21"}) // Add clients
+	clients = append(clients, &Client{Id: "13", Name: "Fred"})
+	clients = append(clients, &Client{Id: "14", Name: "James", Age: "32"})
+	clients = append(clients, &Client{Id: "15", Name: "Danny"})
+	csvContent, err := gocsv.MarshalString(&clients) // Get all clients as CSV string
+	//err = gocsv.MarshalFile(&clients, clientsFile) // Use this to save the CSV back to the file
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(csvContent) // Display all clients as CSV string
 }
 
 func startrecorder() {
@@ -71,5 +92,4 @@ func startrecorder() {
 func main() {
 	//listener()
 	writedatabase()
-	//readdatabase()
 }
