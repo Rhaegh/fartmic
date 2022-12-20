98041d7eb3815d8b1f5feed38b1037939a7a349c
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-20 20:04:17 +0100

    Start all over

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
 	"os/exec"
 )
 
@@ -42,50 +38,6 @@ func startrecorder() {
 	fmt.Println(string(stdout))
 }
 
-func writedata() {
-	fartdata := farts{
-		ID:   2,
-		Name: "Loe",
-		Date: "Datumpje",
-	}
-	finaljson, err := json.Marshal(fartdata)
-	if err != nil {
-		panic(err)
-	}
-	// If the file doesn't exist, create it, or append to the file
-	f, err := os.OpenFile("/home/pi/fartmic/data/db.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if _, err := f.Write([]byte(finaljson)); err != nil {
-		log.Fatal(err)
-	}
-	if err := f.Close(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func readdata() {
-	bytes, err := ioutil.ReadFile("/home/pi/fartmic/data/db.json")
-
-	if err != nil {
-		fmt.Println("Unable to load config file!")
-		return
-	}
-
-	var config farts
-	err = json.Unmarshal(bytes, &config)
-
-	if err != nil {
-		fmt.Println("JSON decode error!")
-		return
-	}
-
-	fmt.Println(config) // {50.3 ~/plugins/ {500 200 500 500}}
-}
-
 func main() {
-	writedata()
-	readdata()
 	recordbutton()
 }
