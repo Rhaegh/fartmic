7d131fe98245c60228110177d3b39861764e2712
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-20 20:34:25 +0100

    cool

--- webpage/script.js
+++ webpage/script.js
@@ -1,5 +1,5 @@
 function send() {
-	countdown()
+	setTimeout(countdown, 1000)
 	const Http = new XMLHttpRequest();
 	const url='http://192.168.72.131:8080/recordbutton';
 	Http.open("GET", url);
@@ -10,8 +10,7 @@ function send() {
 	}
 };
 
-timeLeft = 10;
-
+timeLeft = 5;
 function countdown() {
 	timeLeft--;
 	document.getElementById("seconds").innerHTML = String( timeLeft );
@@ -19,5 +18,3 @@ function countdown() {
 		setTimeout(countdown, 1000);
 	}
 };
-
-setTimeout(countdown, 1000);
