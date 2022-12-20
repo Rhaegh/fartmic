3d674e5fdab6ae3b8a62462bbf417ffc1bda0faf
Rhaegh <97922520+Rhaegh@users.noreply.github.com>
2022-12-20 20:27:32 +0100

    countdown

--- webpage/script.js
+++ webpage/script.js
@@ -8,3 +8,16 @@ function send() {
 	  console.log(Http.responseText)
 	}
 };
+
+function countdown(){
+	var timeleft = 10;
+	var downloadTimer = setInterval(function(){
+	if(timeleft <= 0){
+		clearInterval(downloadTimer);
+		document.getElementById("countdown").innerHTML = "Finished";
+	} else {
+		document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+	}
+	timeleft -= 1;
+	}, 1000);
+}
